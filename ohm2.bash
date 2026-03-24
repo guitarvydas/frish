d7b538d955282d4e35e3efd84da9db49adb30860
Paul Tarvydas <paul.tarvydas@gmail.com>
2026-03-24 09:25:41 -0400

    removed unmatching comment in test.ohm2

--- ohm2.bash
+++ ohm2.bash
@@ -3,12 +3,14 @@ set -eE
 export SHELLOPTS
 shopt -s nullglob
 test=test.ohm2
+echo
 echo '*** bracket matching'
 export PBPWD=$(pwd)
 export PBP="$(pwd)/pbp"
 export PYTHONPATH="${PBP}/kernel:${PYTHONPATH}"
 export PBPCALLER=$PBPWD
 ./pbp/t2t bracketmatch <"${test}"
+echo
 echo '*** 3'
 node nanodsl.3.mjs "${test}"
 # echo '*** 4'
