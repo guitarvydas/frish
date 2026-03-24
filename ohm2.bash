45c65250f4b521615dd380e6a4f602b121855e61
Paul Tarvydas <paul.tarvydas@gmail.com>
2026-03-24 08:29:08 -0400

    bracket matching

--- ohm2.bash
+++ ohm2.bash
@@ -2,8 +2,15 @@
 set -eE
 export SHELLOPTS
 shopt -s nullglob
+test=test.ohm2
+echo '*** bracket matching'
+export PBPWD=$(pwd)
+export PBP="$(pwd)/pbp"
+export PYTHONPATH="${PBP}/kernel:${PYTHONPATH}"
+export PBPCALLER=$PBPWD
+./pbp/t2t bracketmatch <"${test}"
 echo '*** 3'
-node nanodsl.3.mjs test.ohm2
+node nanodsl.3.mjs "${test}"
 # echo '*** 4'
 # node nanodsl.4.mjs test.ohm2 >/dev/null
 # echo '*** 5'
